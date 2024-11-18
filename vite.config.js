f02ca4ae1a749e3a13cfd4c025b4eb7c9343d47c
Khaeril Maswal Zaid <muhammadkhaerilzaid@gmail.com>
2024-11-18 20:32:09 +0800

    nama repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: "/research-pre-experimental/",
+  base: "/pre-experimental-research/",
   plugins: [react()],
 });
