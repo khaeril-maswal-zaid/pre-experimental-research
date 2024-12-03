cbb47c55b024b6dd78b69e1a3d7b9270588fc1cb
Khaeril Maswal Zaid <68152480+khaeril-maswal-zaid@users.noreply.github.com>
2024-12-04 00:02:48 +0800

    Ubah nama repo github

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 import Home from "./Pages/Home";
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/pre-experimental-research/",
+  base: "/webcrafters-research/",
   plugins: [react()],
 });
